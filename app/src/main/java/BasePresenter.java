ae57c2455fb9553d62e05a56469c5411bec01acd
denghang <denghang1989@163.com>
2016-04-21 18:09:48 +0800

    Initial commit

---
+++ app/src/main/java/BasePresenter.java
@@ -0,0 +1,6 @@
+/**
+ * Created by dengh on 2016/4/20.
+ */
+public interface BasePresenter {
+    void start();
+}
---
+++ app/src/main/java/BaseView.java
@@ -0,0 +1,6 @@
+/**
+ * Created by dengh on 2016/4/20.
+ */
+public interface BaseView<T> {
+    void setPresenter(T presenter);
+}
