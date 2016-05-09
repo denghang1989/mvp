bc76893085f731334dd6fcf14a871db377bac425
denghang <denghang1989@163.com>
2016-05-09 21:56:28 +0800

    删除作废的类

--- app/src/main/java/BasePresenter.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by dengh on 2016/4/20.
- */
-public interface BasePresenter {
-    void start();
-}
--- app/src/main/java/BaseView.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by dengh on 2016/4/20.
- */
-public interface BaseView<T> {
-    void setPresenter(T presenter);
-}
