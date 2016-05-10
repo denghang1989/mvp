a1af52cb9e86c24c3cca613dfd57b120f3b40b37
denghang <denghang1989@163.com>
2016-05-10 22:29:55 +0800

    添加一个基类

---
+++ app/src/main/java/fgecctv/com/appdemo/base/BaseActivity.java
@@ -0,0 +1,14 @@
+package fgecctv.com.appdemo.base;
+
+import com.zhy.autolayout.AutoLayoutActivity;
+
+/**
+ * @author denghang
+ * @version V1.0
+ * @Package fgecctv.com.appdemo.base
+ * @Description: (用一句话描述该文件做什么)
+ * @date 2016/5/10 22
+ */
+public class BaseActivity extends AutoLayoutActivity {
+
+}
